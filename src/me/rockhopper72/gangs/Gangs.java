b468fc12dc7f0231cc065e22be6994720576e19b
rockhopper72 <stevenk72@gmail.com>
2014-09-07 16:54:24 -0400

    Added command executors

--- src/me/rockhopper72/gangs/Gangs.java
+++ src/me/rockhopper72/gangs/Gangs.java
@@ -1,13 +1,18 @@
 package me.rockhopper72.gangs;
 
+import me.rockhopper72.commands.GangCommand;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Gangs extends JavaPlugin {
 
+	@Override
 	public void onEnable() {
-
+		GangCommand gangCmd = new GangCommand();
+		this.getCommand("gang").setExecutor(gangCmd);
 	}
 
+	@Override
 	public void onDisable() {
 
 	}
