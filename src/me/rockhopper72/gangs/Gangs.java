f0cfbd3d11995e23594f14fd99daa1bc6c896a66
rockhopper72 <stevenk72@gmail.com>
2014-09-07 16:49:28 -0400

    Started tracking

---
+++ src/me/rockhopper72/gangs/Gangs.java
@@ -0,0 +1,15 @@
+package me.rockhopper72.gangs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Gangs extends JavaPlugin {
+
+	public void onEnable() {
+
+	}
+
+	public void onDisable() {
+
+	}
+
+}
