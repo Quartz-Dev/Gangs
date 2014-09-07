146733b7b1c93cfe33355b2675bde0a05cc1dbda
rockhopper72 <stevenk72@gmail.com>
2014-09-07 16:56:12 -0400

    Actually added command executors

---
+++ src/me/rockhopper72/commands/GangCommand.java
@@ -0,0 +1,15 @@
+package me.rockhopper72.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class GangCommand implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+
+		return false;
+	}
+
+}
