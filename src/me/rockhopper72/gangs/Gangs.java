41f991b64e020d720b173e523bd1f63053b153cf
rockhopper72 <stevenk72@gmail.com>
2014-10-03 01:18:41 -0400

    tested

--- src/me/rockhopper72/gangs/Gangs.java
+++ src/me/rockhopper72/gangs/Gangs.java
@@ -2,6 +2,7 @@ package me.rockhopper72.gangs;
 
 import me.rockhopper72.commands.GangCommand;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Gangs extends JavaPlugin {
@@ -9,7 +10,7 @@ public class Gangs extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		this.getCommand("gang").setExecutor(new GangCommand());
-
+		Bukkit.broadcastMessage("this is a test");
 	}
 
 	@Override
